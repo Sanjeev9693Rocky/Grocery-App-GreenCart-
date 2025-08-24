349da02c2c65df13ba842fcc1db107f4a2d6056d
ranjan <ranjansanjeev2005@email.com>
2025-08-24 17:52:40 +0530

    updated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 
+import { useState } from 'react'
 import './App.css'
 import Appp from './Appp'
+import { useEffect } from 'react'
+import Loading from './components/Loading'
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
   
 
   return (
     <>
-      <Appp />
+      { loading ? <Loading/> : <Appp />}
     </>
   )
 }
