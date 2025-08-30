1fa65d94de0d5ebf10ddec49279709eddf6306ba
ranjan <ranjansanjeev2005@email.com>
2025-08-30 15:48:57 +0530

    updated

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -7,7 +7,7 @@ const BestSeller = () => {
   return (
     <div className='mt-16'>
       <p className='text-2x1 md:text-3x1 font-medium'>Best Sellers</p>
-      <div className='flex justify-center items-center flex-wrap gap-2 md:gap-4 mt-6 mx-[-20px]'>
+      <div className='flex justify-center items-center flex-wrap gap-2 md:gap-4 mt-6 mx-[-200px]'>
         {products.filter((product) => product.inStock).slice(0,5).map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
